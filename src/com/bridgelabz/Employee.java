aaf1737e2b7b0dc03cd145259f763ef64d0b1e3d
Atul <atul43653@gmail.com>
2023-07-21 17:00:53 +0530

    Fourth commit

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -16,6 +16,19 @@ public class Employee {
         int dailyWage = wagePerHrs * fullDayHrs;
         if(randomCheck==1){
             System.out.println("Employee is Present & his daily Wage is = "+dailyWage);
-        }else System.out.println("Employee is Absent. ");
+        }else System.out.println("Employee is Absent & his daily Wage is 0 ");
+
+//  Adding Part time Employee
+        int Is_part_time = 1;
+        int Is_full_time = 2;
+        int randomCheck1 = (int)Math.floor(Math.random() * 10) % 3;
+        if(randomCheck1==1){
+            System.out.println("Part time Employee.");
+        }else{
+            System.out.println("Full time Employee.");
+        }
+        int parttimeHrs = 4;
+        int ptDailyWage = parttimeHrs * wagePerHrs;
+        System.out.println("Part time Employee daily Wage = " + ptDailyWage);
     }
 }
