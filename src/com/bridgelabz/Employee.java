347f3563675af9f6820d5bcd9b9748f661099b12
Atul <atul43653@gmail.com>
2023-07-21 17:10:38 +0530

    fifth commit

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -5,26 +5,35 @@ public class Employee {
         System.out.println("Welcome to Employee Wage Computation Program.");
         // Check Employee is present or not using random
         int randomCheck = (int)Math.floor(Math.random() * 10) % 2;
-        if(randomCheck==1){
-            System.out.println("Employee is Present.");
-        }else{
-            System.out.println("Employee is Absent.");
+        switch (randomCheck) {
+            case 1:
+                System.out.println("Employee is Present");
+                break;
+            case 2:
+                System.out.println("Employee is Absent");
+                break;
         }
         // Calculation of Daily Wage
         int wagePerHrs = 20;
         int fullDayHrs = 8;
         int dailyWage = wagePerHrs * fullDayHrs;
-        if(randomCheck==1){
-            System.out.println("Employee is Present & his daily Wage is = "+dailyWage);
-        }else System.out.println("Employee is Absent & his daily Wage is 0 ");
+        switch (randomCheck) {
+            case 1:
+                System.out.println("Employee is Present & his daily Wage is = " + dailyWage);
+                break;
+            case 2:
+                System.out.println("Employee is Absent & his daily Wage is 0 ");
+        }
 
 //  Adding Part time Employee
         int Is_part_time = 1;
         int Is_full_time = 2;
         int randomCheck1 = (int)Math.floor(Math.random() * 10) % 3;
-        if(randomCheck1==1){
+        switch (randomCheck1){
+            case 1 :
             System.out.println("Part time Employee.");
-        }else{
+            break;
+            case 2:
             System.out.println("Full time Employee.");
         }
         int parttimeHrs = 4;
